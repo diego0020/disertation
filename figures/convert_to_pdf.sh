8debb68f30d9ce65346dc6dbd20c8a08158df2a0
diego <da.angulo39@uniandes.edu.co>
2015-08-14 11:17:22 -0500

    convert to pdf script

---
+++ figures/convert_to_pdf.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+convert_to_pdf () {
+local pdf_name=`basename -s .svg $0`.pdf
+echo cairosvg \"$0\" -o $pdf_name;
+cairosvg "$0" -o "$pdf_name";
+}
+export -f convert_to_pdf
+find . -name *.svg -execdir bash -c convert_to_pdf {} \;
+
